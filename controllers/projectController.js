341ee4d8e1a41ba922fa13b30612659566102892
Nikhil Bhagat <knewboy.nykhil@gmail.com>
2025-01-21 12:59:24 +0545

    Improved Api endpoint [get and create project]

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,31 +1,41 @@
 const Project = require('../models/Project');
 const { sendSuccess, sendError } = require('../utils/response');
 
-// Fetch all projects
+// Get all projects
 exports.ap = async (req, res) => {
   try {
-    const projects = await Project.find();
-    sendSuccess(res, projects);
+    const projects = await Project.find().sort({ createdAt: -1 }); // Sort by createdAt descending
+    sendSuccess(res, { projects });
   } catch (err) {
     sendError(res, 'Failed to fetch projects', 500, err);
   }
 };
 
+
 // Create a new project
 exports.cp = async (req, res) => {
   const { name, description, link } = req.body;
+
+  // Validate request body
   if (!name || !description || !link) {
     return sendError(res, 'All fields are required', 400);
   }
 
   try {
+    // Create and save the new project
     const project = new Project({ name, description, link });
     await project.save();
-    sendSuccess(res, project, 201);
+
+    // Fetch all projects sorted by the most recent first
+    const projects = await Project.find().sort({ createdAt: -1 });
+
+    // Respond with the sorted projects
+    sendSuccess(res, { projects }, 201);
   } catch (err) {
     sendError(res, 'Failed to save project', 500, err);
   }
 };
+
 
 // Update a project
 exports.up = async (req, res) => {
